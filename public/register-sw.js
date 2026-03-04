925d72b34255bbdd27d126c502b3941d0a8ba3fa
Arjuncool99 <i.am.arjun.m.18@gmail.com>
2026-03-04 18:43:01 -0400

    Fix index.html to Lunar Browser

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,18 +1,8 @@
 "use strict";
-/**
- * Distributed with Ultraviolet and compatible with most configurations.
- */
-const stockSW = "/uv/sw.js";
 
-/**
- * List of hostnames that are allowed to run serviceworkers on http://
- */
+const stockSW = "/uv/sw.js";
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
-/**
- * Global util
- * Used in 404.html and index.html
- */
 async function registerSW() {
 	if (!navigator.serviceWorker) {
 		if (
@@ -20,9 +10,7 @@ async function registerSW() {
 			!swAllowedHostnames.includes(location.hostname)
 		)
 			throw new Error("Service workers cannot be registered without https.");
-
 		throw new Error("Your browser doesn't support service workers.");
 	}
-
-	await navigator.serviceWorker.register(stockSW);
+	await navigator.serviceWorker.register('/uv/sw.js', { scope: '/' });
 }
