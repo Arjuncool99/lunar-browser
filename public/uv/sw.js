3eda6a130f6a5940047f99d8295fb5e45033a87d
Arjuncool99 <167709627+Arjuncool99@users.noreply.github.com>
2026-03-08 21:00:30 +0000

    fix: add skipWaiting to SW

--- public/uv/sw.js
+++ public/uv/sw.js
@@ -2,8 +2,19 @@ importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
 importScripts('/uv/uv.sw.js');
 
-const sw = new UVServiceWorker();
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+
+const sw = new UVServiceWorker();
 self.addEventListener('fetch', (event) => {
   if (sw.route(event)) {
     event.respondWith(sw.fetch(event));
